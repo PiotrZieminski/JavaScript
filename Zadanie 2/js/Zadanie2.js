ab0e21691e2f0570487c1fc095131cd12781b4f8
PiotrZieminski <piotrzieminski@outlook.com>
2017-01-26 21:10:17 +0100

    Dodane zadanie 8.3 Kodilla

---
+++ Zadanie 2/js/Zadanie2.js
@@ -0,0 +1,12 @@
+// plik Zadanie2.js
+
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+/*
+    mam tutaj jakiś tekst
+ */
+
+var bogaNieMa = prompt('Kim jesteś ? ');
+alert ('Jesteś Bogiem uświadom to sobie sobie ' + 'Ty jesteś ' + bogaNieMa);
