6d426e7963063dbfeef4ada6b0100722fc4975e4
Michał Gacek <gacek.michael@outlook.com>
2017-01-30 22:10:36 +0100

    Fix

--- Zadanie 7/js/Zadanie7.js
+++ Zadanie 7/js/Zadanie7.js
@@ -1,11 +1,8 @@
-var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach';
-
-var text_1 = 'Zielone słonie';
-var changeText1 = text_1.toUpperCase();
-
-var textAfterChange = text.replace('Papugi', changeText1);
-var textAfterChangefit = textAfterChange.substr(0,45);
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach',
+    text1 = 'Zielone słonie',
+    changeText1 = text_1.toUpperCase(),
+    textAfterChange = text.replace('Papugi', changeText1),
+    textAfterChangefit = textAfterChange.substr(0, text.length / 2);
 
 console.log(textAfterChange);
-
 console.log(textAfterChangefit);
