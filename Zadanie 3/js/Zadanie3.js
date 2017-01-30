2761a1ad1e2d5d6635bf59f1cf490af84b616843
PiotrZieminski <piotrzieminski@outlook.com>
2017-01-30 20:15:16 +0100

    Dodane zadanie 8.4 Kodilla

---
+++ Zadanie 3/js/Zadanie3.js
@@ -0,0 +1,8 @@
+var a = 3;
+var h = 5;
+
+var triangleArea = a*h/2;
+
+console.log('Pole Trójkąta o podstawie a: ' + a + ' i wyskości h: ' + h + ' wynosi: ' + triangleArea);
+
+console.log(triangleArea);
