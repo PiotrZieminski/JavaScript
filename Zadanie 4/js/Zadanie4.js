f7cd3099af4142479a7f12ba1acc3d0d246ba1cf
PiotrZieminski <piotrzieminski@outlook.com>
2017-01-30 20:27:04 +0100

    Dodałem kodilla zadanie 8.5

---
+++ Zadanie 4/js/Zadanie4.js
@@ -0,0 +1,17 @@
+var a = 18;
+var b = -20;
+
+var value = (a*a) + (2*a*b) - (b*b);
+
+value < 0 ? console.log('wynik jest ujemny') : console.log('Wynik jest dodatni');
+
+console.log(value);
+
+var a = 18;
+var b = -2;
+
+var value = (a*a) + (2*a*b) - (b*b);
+
+value < 0 ? console.log('wynik jest ujemny') : console.log('Wynik jest dodatni');
+
+console.log(value);
