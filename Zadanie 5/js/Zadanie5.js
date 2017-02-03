d39599336fc8dea3aa5e55d27e1531d375e18609
PiotrZieminski <piotrzieminski@outlook.com>
2017-02-03 20:38:02 +0100

    Poprawiłem zadanie 8.6

--- Zadanie 5/js/Zadanie5.js
+++ Zadanie 5/js/Zadanie5.js
@@ -1,12 +1,16 @@
 
 function getTriangleArea(a, h) {
-    if (a < 0 && h < 0) {
+    if (a < 0 || h < 0) {
         console.log('Nieprawidłowe dane')
-    }
-    else {
-        var wynikDzialania = a*h/2;
-        return (wynikDzialania);
+    }else {
+        return a*h/2;
     }
 }
-var triangle1Area = getTriangleArea(5, 5);
-console.log(triangle1Area);
+
+console.log(getTriangleArea(10,6));
+
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(15, 15);
+var triangle3Area = getTriangleArea(10, 20);
+console.log(triangle1Area,triangle2Area,triangle3Area);
