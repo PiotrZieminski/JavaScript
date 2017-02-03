ab76f97e21d3e68d7196193214bdae133d20a67b
PiotrZieminski <piotrzieminski@outlook.com>
2017-02-03 20:40:44 +0100

    Poprawiłem zadanie 8.8

--- Zadanie 7/js/Zadanie7.js
+++ Zadanie 7/js/Zadanie7.js
@@ -1,5 +1,5 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach',
-    text1 = 'Zielone słonie',
+    text_1 = 'Zielone słonie',
     changeText1 = text_1.toUpperCase(),
     textAfterChange = text.replace('Papugi', changeText1),
     textAfterChangefit = textAfterChange.substr(0, text.length / 2);
