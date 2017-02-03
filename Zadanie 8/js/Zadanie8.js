d972c6984b03b7eaf692b76b590a257fec588fa1
PiotrZieminski <piotrzieminski@outlook.com>
2017-02-03 20:42:29 +0100

    Poprawiłem zadanie 8.9

--- Zadanie 8/js/Zadanie8.js
+++ Zadanie 8/js/Zadanie8.js
@@ -13,7 +13,7 @@ zrobChoinke(3);
 
 function zrobChoinke1(n) {
     for(i=1;i<=n;i++) {
-        for(j=n-1; j>=i; j--) document.write("&nbsp;");
+        for(j=n-1; j>=i; j--) document.write(" &nbsp;");
         for(j=1; j<=(2*i-1); j++) document.write("*");
         document.write("<br />")
     }
