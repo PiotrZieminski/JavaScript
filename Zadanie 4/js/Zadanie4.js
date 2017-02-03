f16e19ba677f82417977c4aef25bc805ddbc05a1
PiotrZieminski <piotrzieminski@outlook.com>
2017-02-03 20:21:19 +0100

    Poprawiłem zadanie 8.5

--- Zadanie 4/js/Zadanie4.js
+++ Zadanie 4/js/Zadanie4.js
@@ -15,3 +15,21 @@ var value = (a*a) + (2*a*b) - (b*b);
 value < 0 ? console.log('wynik jest ujemny') : console.log('Wynik jest dodatni');
 
 console.log(value);
+
+var a = 0;
+var b = 0;
+
+var value = (a*a) + (2*a*b) - (b*b);
+
+if (value < 0) {
+    console.log('Wynik jest ujemny');
+}
+    else if (value === 0) {
+    console.log('Wynik równy jest 0');
+    }
+    else {
+    console.log('Wynik jest dodatni');
+}
+
+
+console.log(value);
