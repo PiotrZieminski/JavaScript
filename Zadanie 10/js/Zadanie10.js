486b24049c739b46734e64e107a8af5c08ff70f3
PiotrZieminski <piotrzieminski@outlook.com>
2017-02-05 19:22:54 +0100

    Dodalem skonczone zadanie 8.11

--- Zadanie 10/js/Zadanie10.js
+++ Zadanie 10/js/Zadanie10.js
@@ -1,1 +1,44 @@
-// Zadanie 8.11 ;-)
+// Zadanie 8.11 ;-) TEST z materialowy
+
+function checkOnclickEvent(param) {
+    console.log(param);
+}
+var inputElem = document.getElementById('js-inputValue'),
+    imageElem = document.getElementById('js-image');
+
+imageElem.addEventListener('click', function(e) {
+    checkOnclickEvent('image został kliknięty')
+});
+
+inputElem.addEventListener('keypress', function(e) {
+    e.target.value += ' test ';
+});
+
+
+
+
+
+
+//Zadanie 8.11 Eventy 8.11 ;-)
+
+function checkOnClickButtonEvent(parametr) {
+    console.log(parametr);
+}
+var inputList = document.getElementById('js-lista'),
+    inputButton = document.getElementById('js-addElem');
+
+inputList.addEventListener('click', function (e) {
+    checkOnClickButtonEvent('Przycisk zostal kliniety');
+});
+
+var TagName = document.getElementsByTagName('li');
+console.log(TagName);
+
+inputButton.addEventListener('click', function (e) {
+    for (var i = 0 ; i < TagName.length ; i++){
+        var nazwa = inputList[i];
+    }
+        inputList.innerHTML += '<li>' + 'item ' + i + '</li>';
+});
+
+
