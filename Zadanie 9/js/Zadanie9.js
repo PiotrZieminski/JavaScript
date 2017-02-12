5f8e7701b851fa47b98a41d895cbe36b84e27f4e
PiotrZieminski <piotrzieminski@outlook.com>
2017-02-12 11:47:07 +0100

    Poprawiłem zadanie 8.10 ver. 2.0

--- Zadanie 9/js/Zadanie9.js
+++ Zadanie 9/js/Zadanie9.js
@@ -2,7 +2,9 @@ var withMenuButton = document.getElementsByClassName('button');
 
 console.log(withMenuButton);
 
-for (var i = 0 ; i <= withMenuButton.length -1 ; i++){
-    var nazwa = withMenuButton[i];
+var nazwa = "";
+
+for (var i = 0 ; i < withMenuButton.length -1 ; i++){
+    nazwa = withMenuButton[i];
     console.log(nazwa.innerText);
 };
